e52100c8754ac9491588feffcffa940eda1802af
STaylorT <51551018+STaylorT@users.noreply.github.com>
2021-04-27 22:08:36 -0400

    Update simpsh.c

--- simpsh.c
+++ simpsh.c
@@ -1,17 +1,50 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#define BUFFERSIZE 256
+void scanner(char command[]){
+	
+	char str[256];
+	int init_size = strlen(str);
+	char delim[] = " ";
 
+	char *ptr = strtok(str, delim);
 
-scanner(char* command){
+	while(ptr != NULL)
+	{
+		printf("'%s'\n", ptr);
+		ptr = strtok(NULL, delim);
+	}
 
+	/* This loop will show that there are zeroes in the str after tokenizing */
+	for (int i = 0; i < init_size; i++)
+	{
+		printf("%c ", str[i]); /* Convert the character to integer, in this case
+							   the character's ASCII equivalent */
+	}
+	printf("\n");
 }
 
-parser(char* arr[]){
+void parser(char* arr[]){
 
 }
 int main(){
 
+	char str[] = "banana";
+	char exit[] = "exit";
+	
+	int res = strcmp(str,exit);
+	
+	while (res!=0){
+		
+		printf("$");
+		if(fgets(str,BUFFERSIZE, stdin)!=NULL){
+			scanner(str);
+		}
+	}
+  	
+  	res = strcmp(str,exit);
 
 return 0;
 
